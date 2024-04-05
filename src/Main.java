0196cdd3ac70fec1fe2e6518555d672428fcc0a7
flaviojcordeiro <149179836+flaviojcordeiro@users.noreply.github.com>
2024-04-05 08:52:25 -0300

    menu exibição

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+        System.out.println("Bem-vindo ao Spotify!");
+        exibirMenu();
+        System.out.println("Até mais!");
     }
-}
+
+    public static void exibirMenu() {
+        Scanner scanner = new Scanner(System.in);
+        Usuario usuario = null;
+        int opcao;
+        do {
+            System.out.println("\nSelecione uma opção:");
+            System.out.println("1. Cadastrar Usuário");
+            System.out.println("2. Criar Playlist");
+            System.out.println("3. Adicionar Música à Playlist");
+            System.out.println("4. Sair");
+            System.out.print("Opção: ");
+            opcao = scanner.nextInt();
+            scanner.nextLine(); 
+
+        
